613c6ce3c76ab30950314d41dcdabee9623680a5
0lafwan <nawfaltq@gmail.com>
2026-02-02 01:32:02 +0100

    Integrate Keycloak in USER-SERVICE + docker-compose update

---
+++ USER-SERVICE/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.userservice.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @GetMapping("/me")
+    public Map<String, Object> me(Authentication auth) {
+        return Map.of(
+                "username", auth.getName(),
+                "authorities", auth.getAuthorities()
+        );
+    }
+}
